905dbc71d515f03f4eb5f6c10c3f360b1025414b
PankajSharma <pankajeshwara@gmail.com>
2026-05-23 11:32:31 +1200

    Add InfluenceType constants

---
+++ workspace/influenceMiner/src/influenceMiner/InfluenceType.java
@@ -0,0 +1,12 @@
+package influenceMiner;
+
+/*
+ * Basic influence categories used by Influence Miner.
+ */
+public class InfluenceType {
+
+    public static final String STRATEGIC = "strategic";
+    public static final String OPERATIONAL = "operational";
+    public static final String FUNCTIONAL = "functional";
+    public static final String UNKNOWN = "unknown";
+}
