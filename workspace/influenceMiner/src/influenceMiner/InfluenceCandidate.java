a44e464229f88bcfca1d527d1d717d261c9411b3
PankajSharma <pankajeshwara@gmail.com>
2026-05-23 11:43:27 +1200

    Add InfluenceCandidate model

---
+++ workspace/influenceMiner/src/influenceMiner/InfluenceCandidate.java
@@ -0,0 +1,19 @@
+package influenceMiner;
+
+public class InfluenceCandidate {
+
+    public int proposalNumber;
+    public String proposalIdentifier;
+    public String messageId;
+    public String sentence;
+    public String authorRole;
+    public String influenceCategory;
+    public String influenceSubcategory;
+    public String influenceDirection;
+    public double influenceScore;
+    public String finalDecision;
+    public boolean alignsWithDecision;
+
+    public InfluenceCandidate() {
+    }
+}
