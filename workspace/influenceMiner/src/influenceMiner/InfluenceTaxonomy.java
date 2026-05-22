9858588a957dc237048b56cfa844ced56f473042
PankajSharma <pankajeshwara@gmail.com>
2026-05-23 11:43:40 +1200

    Add InfluenceTaxonomy

---
+++ workspace/influenceMiner/src/influenceMiner/InfluenceTaxonomy.java
@@ -0,0 +1,18 @@
+package influenceMiner;
+
+public class InfluenceTaxonomy {
+
+    public static final String STRATEGIC = "strategic";
+    public static final String OPERATIONAL = "operational";
+    public static final String FUNCTIONAL = "functional";
+    public static final String ECONOMIC = "economic";
+    public static final String ORGANIZATIONAL = "organizational";
+    public static final String COALITION = "coalition";
+    public static final String ECOSYSTEM = "ecosystem";
+    public static final String AUTHORITY = "authority";
+    public static final String SECURITY = "security";
+    public static final String COMPATIBILITY = "compatibility";
+    public static final String STANDARDS = "standards";
+    public static final String USER_DEMAND = "user_demand";
+    public static final String UNKNOWN = "unknown";
+}
